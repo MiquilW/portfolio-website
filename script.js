f32190d9472832e2038686fc4aafd17f4b43ef32
MiquilW <michael.welsome@gmail.com>
2022-10-07 14:02:35 -0700

    Pop-ups for projects completed

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function togglePopup() {
 function togglePopupTwo() {
     document.getElementById("popup-2").classList.toggle("active");
 }
+
+function togglePopupThree() {
+    document.getElementById("popup-3").classList.toggle("active");
+}
